d8ae81417bec72c006e8e4d40df3bc32c866f735
aoife kavanagh <nyhankaa@tcd.ie>
2018-07-18 15:37:59 +0000

    added basic routes and views

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Riddle Me This</h1>'
-    
+    #store playerID in a txt file and redirect to user view
+    if request.method == ('POST'):
+        with open('data/players.txt', 'a') as player_list:
+            player_list.writelines(request.form['username'] + '\n')
+        return redirect(request.form['username'])
+    return render_template('index.html')
     
 @app.route('/<username>')
-def player(username):
-    return 'Welcome ' + username
-    
+def user(username):
+    riddles = ask_all_riddles()
+    return render_template('game.html', username=username, riddle_list=riddles)
+
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
