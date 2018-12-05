62e9eb6853950fabdca26e78715ea065f0e75439
aoife kavanagh <nyhankaa@tcd.ie>
2018-12-05 12:50:25 +0000

    turned off debug

--- run.py
+++ run.py
@@ -84,4 +84,4 @@ def user(username):
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=False)
